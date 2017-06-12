fc8772e1c1afceaa5449d9fbb467e7d6db55e35e
Jordan Elver <jordan.elver@gmail.com>
2017-06-12 16:01:13 +0100

    Use dropdowns for problems and solutions

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,15 @@
 class Ticket < ApplicationRecord
+
+  PROBLEMS = [
+    "Order did not arrive",
+    "My order is wrong",
+    "My tights broke"
+  ]
+
+  RESOLUTIONS = [
+    "Please send me a replacement",
+    "Please send me a different item",
+    "Please refund my order"
+  ]
+
 end
