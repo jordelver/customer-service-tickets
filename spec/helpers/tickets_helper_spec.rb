0384dea9a2dfee73b6a2ba44048f03c9c0f36f29
Jordan Elver <jordan.elver@gmail.com>
2017-06-13 14:28:35 +0100

    Add spec for the `#customer_orders` helper method

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 describe TicketsHelper do
+
+  describe "#customer_orders" do
+
+    let(:orders) do
+      [
+        double(id: 1, order_number: "ABC123", description: "Foobar item"),
+        double(id: 2, order_number: "DEF456", description: "Another item"),
+      ]
+    end
+
+    it "outputs the dropdown with order number and description" do
+      expect(helper.customer_orders(orders)).to eq([
+        [ "ABC123 - Foobar item", 1 ],
+        [ "DEF456 - Another item", 2 ],
+      ])
+    end
+  end
 end
 
